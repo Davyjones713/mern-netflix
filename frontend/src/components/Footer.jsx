7e86053face85a79171374482d9b5337676c6d1b
davyjones713 <datoiashvili713@gmail.com>
2024-08-01 22:51:38 +0400

    github fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
           </a>
           . The source code is available on{" "}
           <a
-            href="https://github.com/Davyjones713/netflix-clone"
+            href="https://github.com/Davyjones713/mern-netflix"
             target="_blank"
             rel="noreferrer"
             className="font-medium underline underline-offset-4"
